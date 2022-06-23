c73c214928ea6a0d9a8fe8f78cd3030220e68e64
daydreamboy <wesley4chen@gmail.com>
2022-06-23 21:47:12 +0800

    LogMate: add code

--- LogMate/LogMate/Classes/MainWindowController.swift
+++ LogMate/LogMate/Classes/MainWindowController.swift
@@ -7,7 +7,17 @@
 
 import Cocoa
 
-class MainWindowController: NSWindowController, NSTableViewDataSource {
+class MainWindowController: NSWindowController, NSTableViewDataSource, NSTableViewDelegate {
+    
+    @objc @IBOutlet private dynamic var recordArrayController: NSArrayController?
+    
+    fileprivate enum CellIdentifiers {
+        static let TimeCell = "TimeCell"
+        static let MessageCell = "MessageCell"
+    }
+    
+    
+    var recordList:[String] = []
 
     convenience init() {
         self.init(sender: nil)
@@ -17,12 +27,78 @@ class MainWindowController: NSWindowController, NSTableViewDataSource {
         // Note: NSNibName alias for String
         //  NSNib.Name alias for String
         self.init(windowNibName: NSNib.Name(NSStringFromClass(type(of: self))))
+        
+        self.recordList = [
+            "1 abc",
+            "2 d",
+            "3 d",
+            "4 d",
+            "5 d",
+            "6 d",
+            "7 d",
+            "8 d",
+            "9 d",
+            "10 d",
+        ]
     }
     
     override func windowDidLoad() {
         super.windowDidLoad()
-
-        // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
     }
     
+    // MARK: NSTableViewDataSource
+    func numberOfRows(in tableView: NSTableView) -> Int {
+        return self.recordList.count
+    }
+    
+    // MARK: NSTableViewDelegate
+    func tableView(_ tableView: NSTableView, viewFor tableColumn: NSTableColumn?, row: Int) -> NSView? {
+        // @see https://medium.com/@kicsipixel/very-simple-view-based-nstableview-in-swift-5-using-model-134c5f4c12ee
+        guard let cell = tableView.makeView(withIdentifier: tableColumn!.identifier, owner: self) as? NSTableCellView else {
+            return nil
+        }
+        
+        let text = self.recordList[row]
+        let parts:[String] = text .components(separatedBy: " ")
+        
+        if (tableColumn?.identifier)!.rawValue == CellIdentifiers.TimeCell {
+            cell.textField?.stringValue = parts.first!
+        } else if (tableColumn?.identifier)!.rawValue == CellIdentifiers.MessageCell {
+            cell.textField?.stringValue = parts.last!
+        }
+        
+        return cell
+        
+        
+        /*
+         var text: String = ""
+         var someCellIdentifier: String?
+         
+         // 2
+         if tableColumn == tableView.tableColumns[0] {
+             text = self.recordList[row]
+             someCellIdentifier = CellIdentifiers.TimeCell
+         } else if tableColumn == tableView.tableColumns[1] {
+             someCellIdentifier = CellIdentifiers.MessageCell
+         }
+         
+         // 3
+         // https://www.raywenderlich.com/830-macos-nstableview-tutorial
+         if let cellIdentifier = someCellIdentifier, let cell = tableView.makeView(withIdentifier: NSUserInterfaceItemIdentifier(rawValue: cellIdentifier), owner: nil) as? NSTableCellView {
+             cell.textField?.stringValue = text
+             return cell
+         }
+         
+         return nil
+         */
+    }
+    
+    func tableViewSelectionDidChange(_ notification: Notification) {
+        print("did change")
+    }
+    
+    func tableView(_ tableView: NSTableView, didClick tableColumn: NSTableColumn) {
+        print("didClick \(tableColumn.identifier.rawValue)")
+    }
+
 }
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # HelloMacOSApp
 Learn development of MacOS app
+
+
+
+
+
+NSTableView
+
+https://medium.com/@kicsipixel/very-simple-view-based-nstableview-in-swift-5-using-model-134c5f4c12ee
+
+
+
+add constraints
+
+https://stackoverflow.com/a/66386655
+

267f137a2b71e61e95045e8a95e93e8836f005e6
daydreamboy <wesley4chen@gmail.com>
2022-06-26 18:18:34 +0800

    add HelloStoryboard

---
+++ HelloStoryboard/HelloStoryboard/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  HelloStoryboard
+//
+//  Created by wesley_chen on 2022/6/26.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
---
+++ HelloStoryboard/README.md
@@ -0,0 +1,11 @@
+# HelloStoryboard
+[TOC]
+
+## 1、介绍Storyboard
+
+
+
+
+
+## 2、使用Storyboard
+

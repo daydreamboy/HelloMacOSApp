0f63b1202c84673debe2aee149b2a3b98d6214a0
daydreamboy <wesley4chen@gmail.com>
2023-10-14 22:27:30 +0800

    HelloSparkle: add new configuration

--- HelloSparkle/HelloSparkle/RunScripts/pack.sh
+++ HelloSparkle/HelloSparkle/RunScripts/pack.sh
@@ -3,6 +3,18 @@
 set -e
 set -x
 
+if [ "false" == "${THIS_SCRIPT_ALREADY_INVOKED:-false}" ]; then
+  export THIS_SCRIPT_ALREADY_INVOKED="true"
+else
+  exit 0
+fi
+
+if [ "${CONFIGURATION}" != "Packaging" ]; then
+  echo "[Warning] current scheme is not Release, just return"
+  exit 0
+fi
+
+
 # Configure here...
 XCWORKSPACE_NAME=HelloSparkle
 SCHEME_NAME=HelloSparkle
@@ -12,22 +24,17 @@ WORKSPACE_PATH=${PROJECT_DIR}/${XCWORKSPACE_NAME}.xcworkspace
 PACKAGE_PATH=${PROJECT_DIR}/${PACKAGE_FOLDER}
 
 build_number=$(date +%Y%m%d/%H:%m:%S)
-info_plist=$INFOPLIST_FILE
 
 echo "[Info] $(xcodebuild -version)"
 
-if [ "${CONFIGURATION}" != "Release" ]; then
-  echo "[Warning] current scheme is not Release, just return"
-  exit 0
-fi
-
 # Step1:
-/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $build_number" "${info_plist}"
+/usr/libexec/PlistBuddy -c "Set :CFBundleVersion $build_number" "${INFOPLIST_FILE}"
 
 #xcodebuild clean -workspace "${WORKSPACE_PATH}" -scheme "${SCHEME_NAME}" -configuration "${CONFIGURATION}" || exit
 
-#xcodebuild archive -workspace "${WORKSPACE_PATH}" -scheme ${SCHEME_NAME} -archivePath "${ARCHIVE_PATH}" -quiet || exit
+xcodebuild archive -workspace "${WORKSPACE_PATH}" -scheme ${SCHEME_NAME} -archivePath "${PACKAGE_PATH}/${SCHEME_NAME}.xcarchive" -quiet || exit 1
 #xcodebuild -exportArchive -archivePath "${PACKAGE_PATH}/${SCHEME_NAME}.xcarchive" -exportPath "${PACKAGE_PATH}" -exportOptionsPlist ./ExportOptions.plist -quiet || exit
+xcodebuild -exportArchive -archivePath "${PACKAGE_PATH}/${SCHEME_NAME}.xcarchive" -exportPath "${PACKAGE_PATH}" -quiet || exit
 
 if [ -e "${PROJECT_DIR}/${PACKAGE_FOLDER}/${PRODUCT_NAME}.app" ]; then
   echo "[Info] export ipa file success"
